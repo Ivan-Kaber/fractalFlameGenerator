c12c6a87e1ba691ad172bcbdfbd0d997cf999fb0
Ivan Kabernyuk <kabernyuk.dev@gmail.com>
2024-12-05 20:17:37 +0300

    feat: Add Transformation interface

---
+++ src/main/java/backend/academy/fractal/flame/transformations/Transformation.java
@@ -0,0 +1,8 @@
+package backend.academy.fractal.flame.transformations;
+
+import backend.academy.fractal.flame.model.Point;
+import java.util.function.Function;
+
+public interface Transformation extends Function<Point, Point> {
+
+}
