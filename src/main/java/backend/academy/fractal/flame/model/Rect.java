5293a1d4044993d2e43926cb125f994a778fd3d7
Ivan Kabernyuk <kabernyuk.dev@gmail.com>
2024-12-05 20:15:19 +0300

    feat: Add Rect record

---
+++ src/main/java/backend/academy/fractal/flame/model/Rect.java
@@ -0,0 +1,7 @@
+package backend.academy.fractal.flame.model;
+
+public record Rect(double x, double y, double width, double height) {
+    public boolean contains(Point point) {
+        return point.x() > x && point.x() < x + width && point.y() > y && point.y() < y + height;
+    }
+}
