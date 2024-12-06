b2278c76eaf5246b4595459976b168048c6a4512
Ivan Kabernyuk <kabernyuk.dev@gmail.com>
2024-12-06 17:27:45 +0300

    fix: add Point as field

--- src/main/java/backend/academy/fractal/flame/model/Pixel.java
+++ src/main/java/backend/academy/fractal/flame/model/Pixel.java
@@ -1,4 +1,24 @@
 package backend.academy.fractal.flame.model;
 
-public record Pixel(Rgb rgb, int hitCount) {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Pixel {
+    private Point point;
+    private Rgb rgb;
+    private Integer hitCount;
+
+    public Pixel(Point point) {
+        this.point = point;
+        this.rgb = new Rgb(0, 0, 0);
+        this.hitCount = 0;
+    }
+
+    public void increaseHitCount() {
+        hitCount++;
+    }
 }
