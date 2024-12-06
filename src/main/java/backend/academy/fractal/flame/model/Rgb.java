435645008fd67c8c3b442065eca4e2bb309e2334
Ivan Kabernyuk <kabernyuk.dev@gmail.com>
2024-12-06 19:15:03 +0300

    fix: Add arithmetical method

--- src/main/java/backend/academy/fractal/flame/model/Rgb.java
+++ src/main/java/backend/academy/fractal/flame/model/Rgb.java
@@ -1,4 +1,18 @@
 package backend.academy.fractal.flame.model;
 
-public record Rgb (int red, int green, int blue) {
+public record Rgb(int red, int green, int blue) {
+
+    public Rgb sum(Rgb newRgb) {
+        int red = this.red + newRgb.red();
+        int green = this.green + newRgb.green();
+        int blue = this.blue + newRgb.blue();
+        return new Rgb(red, green, blue);
+    }
+
+    public Rgb divideByTwo() {
+        return new Rgb(
+            this.red / 2,
+            this.green / 2,
+            this.blue / 2);
+    }
 }
