76e9b68877286cbe075c250391154ecf2fcbf8db
Ivan Kabernyuk <kabernyuk.dev@gmail.com>
2024-12-06 19:12:56 +0300

    feat: Add contains method

--- src/main/java/backend/academy/fractal/flame/model/Rect.java
+++ src/main/java/backend/academy/fractal/flame/model/Rect.java
@@ -2,6 +2,6 @@ package backend.academy.fractal.flame.model;
 
 public record Rect(double x, double y, double width, double height) {
     public boolean contains(Point point) {
-        return point.x() > x && point.x() < x + width && point.y() > y && point.y() < y + height;
+        return point.x() >= x && point.x() <= width && point.y() >= y && point.y() <= height;
     }
 }
