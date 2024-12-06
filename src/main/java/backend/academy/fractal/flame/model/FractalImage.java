2573c5380cc6847834568b16dce180052ba46ea3
Ivan Kabernyuk <kabernyuk.dev@gmail.com>
2024-12-06 17:23:03 +0300

    fix:make class instead record

--- src/main/java/backend/academy/fractal/flame/model/FractalImage.java
+++ src/main/java/backend/academy/fractal/flame/model/FractalImage.java
@@ -1,18 +1,52 @@
+/*package backend.academy.fractal.flame.model;
+
+import backend.academy.fractal.flame.FractalRenderer;
+import backend.academy.fractal.flame.transformations.Transformation;
+import lombok.Setter;
+
+@Setter
+public class FractalImage {
+    private Pixel[][] histogram;
+    private int width;
+    private int height;
+
+    public FractalImage(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public Pixel[][] getHistogram() {
+        return histogram;
+    }
+}*/
+
 package backend.academy.fractal.flame.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import java.util.Arrays;
 
-public record FractalImage(Pixel[] data, int width, int height) {
-    public static FractalImage create(int width, int height) {
-        Pixel[] pixels = new Pixel[width * height];
-        Arrays.fill(pixels, new Pixel(new Rgb(0, 0, 0), 0));
-        return new FractalImage(pixels, width, height);
+@Getter
+@Setter
+public class FractalImage {
+    private Pixel[][] data;
+    private final int width;
+    private final int height;
+
+    public FractalImage(int width, int height) {
+        this.width = width;
+        this.height = height;
     }
 
     private boolean contains(int x, int y) {
         return x >= 0 && x < width && y >= 0 && y < height;
-    }
-    private Pixel pixel(int x, int y) {
-        return data[x + y * width];
     }
 }
