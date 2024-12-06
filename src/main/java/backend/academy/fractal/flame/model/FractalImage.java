e0a3edaa27912225c3081bc3475f77a876f11d65
Ivan Kabernyuk <kabernyuk.dev@gmail.com>
2024-12-06 17:26:30 +0300

    fix: cosmetic fixes

--- src/main/java/backend/academy/fractal/flame/model/FractalImage.java
+++ src/main/java/backend/academy/fractal/flame/model/FractalImage.java
@@ -1,33 +1,3 @@
-/*package backend.academy.fractal.flame.model;
-
-import backend.academy.fractal.flame.FractalRenderer;
-import backend.academy.fractal.flame.transformations.Transformation;
-import lombok.Setter;
-
-@Setter
-public class FractalImage {
-    private Pixel[][] histogram;
-    private int width;
-    private int height;
-
-    public FractalImage(int width, int height) {
-        this.width = width;
-        this.height = height;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public Pixel[][] getHistogram() {
-        return histogram;
-    }
-}*/
-
 package backend.academy.fractal.flame.model;
 
 import lombok.Getter;
