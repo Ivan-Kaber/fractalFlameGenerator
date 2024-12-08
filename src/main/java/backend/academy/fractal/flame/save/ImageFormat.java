d59f82107b25043a95245543967e825c18e3a601
Ivan Kabernyuk <kabernyuk.dev@gmail.com>
2024-12-08 17:39:49 +0300

    feat: Add utility methods for enum ImageFormat

--- src/main/java/backend/academy/fractal/flame/save/ImageFormat.java
+++ src/main/java/backend/academy/fractal/flame/save/ImageFormat.java
@@ -1,11 +1,34 @@
 package backend.academy.fractal.flame.save;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public enum ImageFormat {
     JPEG("jpg"), BMP("bmp"), PNG("png");
 
+    private final Integer id;
     private final String extension;
 
     ImageFormat(String extension) {
+        this.id = ordinal() + 1;
         this.extension = extension;
+    }
+
+    public static String getOrderView() {
+        return Arrays.stream(values())
+            .map(c -> String.format("%d) %s", c.id, c.extension))
+            .collect(Collectors.joining(System.lineSeparator()));
+    }
+
+    public static ImageFormat getById(int id) {
+        return Arrays.stream(values())
+            .filter(c -> c.id == id)
+            .findFirst()
+            .orElse(null);
+    }
+
+    public static boolean contains(String id) {
+        return Arrays.stream(values())
+            .anyMatch(c -> String.valueOf(c.id).equals(id));
     }
 }
