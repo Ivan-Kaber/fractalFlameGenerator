2872921223b8ce092cd0535e0a966745d68227dd
Ivan Kabernyuk <kabernyuk.dev@gmail.com>
2024-12-08 17:41:42 +0300

    fix: fix main

--- src/main/java/backend/academy/fractal/flame/Main.java
+++ src/main/java/backend/academy/fractal/flame/Main.java
@@ -1,7 +1,18 @@
 package backend.academy.fractal.flame;
 
+import backend.academy.fractal.flame.interaction.ConsoleInterface;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.io.PrintStream;
+import java.util.Scanner;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+@SuppressFBWarnings("DM_DEFAULT_ENCODING")
 public class Main {
     public static void main(String[] args) {
-
+        PrintStream out = new PrintStream(System.out);
+        Scanner scanner = new Scanner(System.in);
+        ConsoleInterface consoleInterface = new ConsoleInterface(scanner, out);
+        consoleInterface.processUserInteraction();
     }
 }
