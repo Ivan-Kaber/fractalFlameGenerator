59df35c6f7704ef9318983f67bd4ef0c537ddeea
Ivan Kabernyuk <kabernyuk.dev@gmail.com>
2024-12-08 12:33:07 +0300

    fix: to realize abstract methods for singlethread renderer

--- src/main/java/backend/academy/fractal/flame/renderer/SingleThreadFractalRenderer.java
+++ src/main/java/backend/academy/fractal/flame/renderer/SingleThreadFractalRenderer.java
@@ -1,104 +1,42 @@
 package backend.academy.fractal.flame.renderer;
 
-import backend.academy.fractal.flame.AffineTransformation;
-import backend.academy.fractal.flame.model.FractalImage;
-import backend.academy.fractal.flame.model.Pixel;
 import backend.academy.fractal.flame.model.Point;
-import backend.academy.fractal.flame.model.Rect;
-import backend.academy.fractal.flame.model.Rgb;
+import backend.academy.fractal.flame.transformations.AffineTransformation;
 import backend.academy.fractal.flame.transformations.Transformation;
-import java.util.Random;
+import java.security.SecureRandom;
 
-public class SingleThreadFractalRenderer implements FractalRenderer {
-    private static final Rect WORLD = new Rect(-1.0, -1.0, 1.0, 1.0);
-    private static final Random RANDOM = new Random();
-    private static final Integer MISSED_ITERATIONS = -20;
-    private FractalImage fractalImage;
+public class SingleThreadFractalRenderer extends FractalRenderer {
+    private static final SecureRandom RANDOM = new SecureRandom();
 
-    public void generate(
-        FractalImage image,
-        int samples,
-        int iterPerSamples,
-        int countOfSymmetric,
-        int countOfAffineCoefficients,
-        Transformation... transformations
+    @Override
+    void renderSamples(
+        int samples, int iterPerSamples, int countOfSymmetric,
+        Transformation[] transformations, AffineTransformation[] affineTransformation
     ) {
-        this.fractalImage = image;
-        AffineTransformation[] affineTransformation = new AffineTransformation[countOfAffineCoefficients];
-        for (int i = 0; i < affineTransformation.length; i++) {
-            affineTransformation[i] = new AffineTransformation().getAffineTransformation();
-        }
-
         for (int i = 0; i < samples; i++) {
             render(iterPerSamples, countOfSymmetric, affineTransformation, transformations);
         }
     }
 
-    private void render(
-        int iterPerSamples,
-        int countOfSymmetric,
-        AffineTransformation[] affineTransformation,
-        Transformation[] transformations
+    @Override
+    Point applyAffineTransformation(
+        Transformation[] transformations, Point point,
+        AffineTransformation affineTransformation
     ) {
-        Point point = getRandomPoint();
-
-        for (int i = MISSED_ITERATIONS; i < iterPerSamples; i++) {
-            int randomAffineIndex = RANDOM.nextInt(0, affineTransformation.length);
-            point = affineTransformation[randomAffineIndex].applyTransformation(point);
-            point = transformations[RANDOM.nextInt(0, transformations.length)].apply(point);
-
-            if (i > 0) {
-                double theta2 = 0;
-                for (int j = 0; j < countOfSymmetric; theta2 += (Math.PI * 2 / countOfSymmetric), j++) {
-                    point = rotate(point, theta2);
-
-                    if (!WORLD.contains(point)) {
-                        continue;
-                    }
-
-                    int width = fractalImage.width();
-                    int height = fractalImage.height();
-                    int x1 = width - (int) ((WORLD.width() - point.x()) / (2 * WORLD.width()) * width);
-                    int y1 = height - (int) ((WORLD.height() - point.y()) / (2 * WORLD.height()) * height);
-                    Point point1 = new Point(x1, y1);
-
-                    if (point1.x() < width && point1.y() < height) {
-                        Pixel pixel = setPixelColor(affineTransformation, point1, randomAffineIndex);
-                        pixel.increaseHitCount();
-                    }
-                }
-            }
-        }
+        point = affineTransformation.applyTransformation(point);
+        point = transformations[RANDOM.nextInt(0, transformations.length)].apply(point);
+        return point;
     }
 
-    private Pixel setPixelColor(
-        AffineTransformation[] affineTransformations,
-        Point point1,
-        int randomAffineTransformation
-    ) {
-        Pixel pixel = fractalImage.data()[(int) point1.x()][(int) point1.y()];
-        Rgb rgb;
-        if (pixel.hitCount() == 0) {
-            rgb = affineTransformations[randomAffineTransformation].color();
-        } else {
-            rgb = pixel
-                .rgb()
-                .sum(affineTransformations[randomAffineTransformation].color())
-                .divideByTwo();
-        }
-        pixel.rgb(rgb);
-        return pixel;
+    @Override
+    int getRandomAffineIndex(AffineTransformation[] affineTransformation) {
+        return RANDOM.nextInt(0, affineTransformation.length);
     }
 
-    private Point getRandomPoint() {
-        double x = RANDOM.nextDouble(WORLD.x(), WORLD.width());
-        double y = RANDOM.nextDouble(WORLD.y(), WORLD.height());
+    @Override
+    Point getRandomPoint() {
+        double x = RANDOM.nextDouble(world.x(), world.width());
+        double y = RANDOM.nextDouble(world.y(), world.height());
         return new Point(x, y);
-    }
-
-    public Point rotate(Point point, double theta) {
-        double xRotated = point.x() * Math.cos(theta) - point.y() * Math.sin(theta);
-        double yRotated = point.x() * Math.sin(theta) + point.y() * Math.cos(theta);
-        return new Point(xRotated, yRotated);
     }
 }
